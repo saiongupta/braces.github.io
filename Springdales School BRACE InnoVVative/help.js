45ce36338cad225602ced1492a5dca4567a9ae90
Saion Gupta <84860026+saiongupta@users.noreply.github.com>
2024-08-02 10:04:19 +0530

    Add files via upload

---
+++ Springdales School BRACE InnoVVative/help.js
@@ -0,0 +1,16 @@
+function dialSOS() {
+    window.location.href = "tel:108";
+}
+
+
+const header = document.querySelector("header");
+const menuBtn = document.querySelector("#menu-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  header.classList.toggle("show-mobile-menu");
+})
+
+closeMenuBtn.addEventListener("click", () => {
+  menuBtn.click()
+})
